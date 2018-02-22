2135aae45d51df43585d148c47d4462dbe9e034a
oleg291291@mail.ru <oleg291291@mail.ru>
2018-02-22 10:55:44 +0300

    half!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  // check('()', [['(', ')']]) // -> true
+  // check('())(', [['(', ')']]) // -> false
+  var stack = [];
+  var result;
+  for (var i = 0; i < str.length; i++) {
+
+    
+    if (str[i] === '(') {
+      stack.push(str[i]);
+    }
+    
+    else {
+      if (str[i] === ')' && (stack[stack.length - 1] !== '(' || stack.lenght < 1)) {
+        return false;
+      }
+      else {
+        stack.pop();
+      }
+    }
+  }
+if (stack.length == 0){
+  return true;
+}
+
 }
