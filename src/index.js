f1fd9a4f360cb631ea0040af96508c0590e7f436
oleg291291@mail.ru <oleg291291@mail.ru>
2018-02-22 11:47:15 +0300

    10/10

--- src/index.js
+++ src/index.js
@@ -4,17 +4,26 @@ module.exports = function check(str, bracketsConfig) {
   // check('())(', [['(', ')']]) // -> false
   var stack = [];
 
+  var openBr, openBr2, openBr3, closeBr, closeBr2, closeBr3;
+  openBr = bracketsConfig[0][0];
+  closeBr = bracketsConfig[0][1];
+  openBr2 = bracketsConfig[0][0];
+  closeBr2 = bracketsConfig[0][1];
 
-  var openBr = bracketsConfig[0][0];
-  var closeBr = bracketsConfig[0][1];
+  if (bracketsConfig.length > 1) {
+    openBr2 = bracketsConfig[1][0];
+    closeBr2 = bracketsConfig[1][1];
+
+  }
+
 
   for (var i = 0; i < str.length; i++) {
 
-    
+
     if (str[i] === openBr) {
       stack.push(str[i]);
     }
-    
+
     else {
       if (str[i] === closeBr && (stack[stack.length - 1] !== openBr || stack.lenght < 1)) {
         return false;
@@ -23,13 +32,26 @@ module.exports = function check(str, bracketsConfig) {
         stack.pop();
       }
     }
+
+    if (str[i] === openBr2) {
+      stack.push(str[i]);
+    }
+
+    else {
+      if (str[i] === closeBr2 && (stack[stack.length - 1] !== openBr2 || stack.lenght < 1)) {
+        return false;
+      }
+      else {
+        stack.pop();
+      }
+    }
   }
-  
-  
-if (stack.length == 0){
-  return true;
-}
-  
+
+
+  if (stack.length == 0) {
+    return true;
+  }
+
 
 }
 
